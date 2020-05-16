f175ce28732577bfbc578aac18f31529fc451cb5
Jon <jonanthawillian@gmail.com>
2020-05-15 22:53:41 -0300

    Calculadora básica feita em Javascript, Versão 1.0

---
+++ Calculadora_Em_Javascript/function.js
@@ -0,0 +1,204 @@
+var PrimeiroNumero=0;
+var SegundoNumero=0;
+var Operador=0;
+var Valor=0;
+
+/* Funções para os números */
+
+function Conta0() {
+    Valor += document.getElementById('Botao0').value;
+    document.getElementById('Textodocorpo').innerText = Valor;
+    if(Operador==1){
+      document.getElementById('Textodocorpo').innerText = PrimeiroNumero+ " + " + Valor;
+    }else if(Operador==2){
+      document.getElementById('Textodocorpo').innerText = PrimeiroNumero+ " - " + Valor;
+    }else if(Operador==3){
+      document.getElementById('Textodocorpo').innerText = PrimeiroNumero+ " * " + Valor;
+    }else if(Operador==4){
+      document.getElementById('Textodocorpo').innerText = PrimeiroNumero+ " / " + Valor;
+    }
+};
+function Conta1() {
+    Valor += document.getElementById('Botao1').value;
+    document.getElementById('Textodocorpo').innerText = Valor;
+    if(Operador==1){
+      document.getElementById('Textodocorpo').innerText = PrimeiroNumero+ " + " + Valor;
+    }else if(Operador==2){
+      document.getElementById('Textodocorpo').innerText = PrimeiroNumero+ " - " + Valor;
+    }else if(Operador==3){
+      document.getElementById('Textodocorpo').innerText = PrimeiroNumero+ " * " + Valor;
+    }else if(Operador==4){
+      document.getElementById('Textodocorpo').innerText = PrimeiroNumero+ " / " + Valor;
+    }
+};
+function Conta2() {
+    Valor += document.getElementById('Botao2').value;
+    document.getElementById('Textodocorpo').innerText = Valor;
+    if(Operador==1){
+      document.getElementById('Textodocorpo').innerText = PrimeiroNumero+ " + " + Valor;
+    }else if(Operador==2){
+      document.getElementById('Textodocorpo').innerText = PrimeiroNumero+ " - " + Valor;
+    }else if(Operador==3){
+      document.getElementById('Textodocorpo').innerText = PrimeiroNumero+ " * " + Valor;
+    }else if(Operador==4){
+      document.getElementById('Textodocorpo').innerText = PrimeiroNumero+ " / " + Valor;
+    }
+};
+function Conta3() {
+    Valor += document.getElementById('Botao3').value;
+    document.getElementById('Textodocorpo').innerText = Valor;
+    if(Operador==1){
+      document.getElementById('Textodocorpo').innerText = PrimeiroNumero+ " + " + Valor;
+    }else if(Operador==2){
+      document.getElementById('Textodocorpo').innerText = PrimeiroNumero+ " - " + Valor;
+    }else if(Operador==3){
+      document.getElementById('Textodocorpo').innerText = PrimeiroNumero+ " * " + Valor;
+    }else if(Operador==4){
+      document.getElementById('Textodocorpo').innerText = PrimeiroNumero+ " / " + Valor;
+    }
+};
+function Conta4() {
+    Valor += document.getElementById('Botao4').value;
+    document.getElementById('Textodocorpo').innerText = Valor;
+    if(Operador==1){
+      document.getElementById('Textodocorpo').innerText = PrimeiroNumero+ " + " + Valor;
+    }else if(Operador==2){
+      document.getElementById('Textodocorpo').innerText = PrimeiroNumero+ " - " + Valor;
+    }else if(Operador==3){
+      document.getElementById('Textodocorpo').innerText = PrimeiroNumero+ " * " + Valor;
+    }else if(Operador==4){
+      document.getElementById('Textodocorpo').innerText = PrimeiroNumero+ " / " + Valor;
+    }
+};
+function Conta5() {
+    Valor += document.getElementById('Botao5').value;
+    document.getElementById('Textodocorpo').innerText = Valor;
+    if(Operador==1){
+      document.getElementById('Textodocorpo').innerText = PrimeiroNumero+ " + " + Valor;
+    }else if(Operador==2){
+      document.getElementById('Textodocorpo').innerText = PrimeiroNumero+ " - " + Valor;
+    }else if(Operador==3){
+      document.getElementById('Textodocorpo').innerText = PrimeiroNumero+ " * " + Valor;
+    }else if(Operador==4){
+      document.getElementById('Textodocorpo').innerText = PrimeiroNumero+ " / " + Valor;
+    }
+};
+function Conta6() {
+    Valor += document.getElementById('Botao6').value;
+    document.getElementById('Textodocorpo').innerText = Valor;
+    if(Operador==1){
+      document.getElementById('Textodocorpo').innerText = PrimeiroNumero+ " + " + Valor;
+    }else if(Operador==2){
+      document.getElementById('Textodocorpo').innerText = PrimeiroNumero+ " - " + Valor;
+    }else if(Operador==3){
+      document.getElementById('Textodocorpo').innerText = PrimeiroNumero+ " * " + Valor;
+    }else if(Operador==4){
+      document.getElementById('Textodocorpo').innerText = PrimeiroNumero+ " / " + Valor;
+    }
+};
+function Conta7() {
+    Valor += document.getElementById('Botao7').value;
+    document.getElementById('Textodocorpo').innerText = Valor;
+    if(Operador==1){
+      document.getElementById('Textodocorpo').innerText = PrimeiroNumero+ " + " + Valor;
+    }else if(Operador==2){
+      document.getElementById('Textodocorpo').innerText = PrimeiroNumero+ " - " + Valor;
+    }else if(Operador==3){
+      document.getElementById('Textodocorpo').innerText = PrimeiroNumero+ " * " + Valor;
+    }else if(Operador==4){
+      document.getElementById('Textodocorpo').innerText = PrimeiroNumero+ " / " + Valor;
+    }
+};
+function Conta8() {
+    Valor += document.getElementById('Botao8').value;
+    document.getElementById('Textodocorpo').innerText = Valor;
+    if(Operador==1){
+      document.getElementById('Textodocorpo').innerText = PrimeiroNumero+ " + " + Valor;
+    }else if(Operador==2){
+      document.getElementById('Textodocorpo').innerText = PrimeiroNumero+ " - " + Valor;
+    }else if(Operador==3){
+      document.getElementById('Textodocorpo').innerText = PrimeiroNumero+ " * " + Valor;
+    }else if(Operador==4){
+      document.getElementById('Textodocorpo').innerText = PrimeiroNumero+ " / " + Valor;
+    }
+};
+function Conta9() {
+    Valor += document.getElementById('Botao9').value;
+    document.getElementById('Textodocorpo').innerText = Valor;
+    if(Operador==1){
+      document.getElementById('Textodocorpo').innerText = PrimeiroNumero+ " + " + Valor;
+    }else if(Operador==2){
+      document.getElementById('Textodocorpo').innerText = PrimeiroNumero+ " - " + Valor;
+    }else if(Operador==3){
+      document.getElementById('Textodocorpo').innerText = PrimeiroNumero+ " * " + Valor;
+    }else if(Operador==4){
+      document.getElementById('Textodocorpo').innerText = PrimeiroNumero+ " / " + Valor;
+    }
+};
+
+function PI(){
+    Valor = 3.14159;
+    document.getElementById('Textodocorpo').innerText = Valor;
+    if(Operador==1){
+      document.getElementById('Textodocorpo').innerText = PrimeiroNumero+ " + " + Valor;
+    }else if(Operador==2){
+      document.getElementById('Textodocorpo').innerText = PrimeiroNumero+ " - " + Valor;
+    }else if(Operador==3){
+      document.getElementById('Textodocorpo').innerText = PrimeiroNumero+ " * " + Valor;
+    }else if(Operador==4){
+      document.getElementById('Textodocorpo').innerText = PrimeiroNumero+ " / " + Valor;
+    }
+}
+/* Funcões para os operadores */
+
+function OperadorSoma() {
+    Operador = document.getElementById('BotaoAdicao').value;
+    document.getElementById('Textodocorpo').innerText += "+";
+    PrimeiroNumero = Number(Valor);
+    Valor = 0;
+};
+
+function OperadorSubtracao() {
+    Operador = document.getElementById('BotaoSubtracao').value;
+    document.getElementById('Textodocorpo').innerText += "-";
+    PrimeiroNumero = Number(Valor);
+    Valor = 0;
+};
+
+function OperadorMultiplicacao() {
+    Operador = document.getElementById('BotaoMultiplicacao').value;
+    document.getElementById('Textodocorpo').innerText += "*";
+    PrimeiroNumero = Number(Valor);
+    Valor = 0;
+};
+
+function OperadorDivisao() {
+    Operador = document.getElementById('BotaoDivisao').value;
+    document.getElementById('Textodocorpo').innerText += "/";
+    PrimeiroNumero = Number(Valor);
+    Valor = 0;
+};
+
+
+/* função de Resultado e Apagar */
+
+function Resultado(){
+  SegundoNumero = Number(Valor);
+  if(Operador==1){
+    document.getElementById('Textodocorpo').innerText = PrimeiroNumero+SegundoNumero;
+  }else if(Operador==2){
+    document.getElementById('Textodocorpo').innerText = PrimeiroNumero-SegundoNumero;
+  }else if(Operador==3){
+    document.getElementById('Textodocorpo').innerText = PrimeiroNumero*SegundoNumero;
+  }else if(Operador==4){
+    document.getElementById('Textodocorpo').innerText = PrimeiroNumero/SegundoNumero;
+  }
+};
+
+function Apagar() {
+  document.getElementById('Textodocorpo').innerText = null;
+  PrimeiroNumero=0;
+  SegundoNumero=0;
+  Valor=0;
+  Operador=0;
+};
